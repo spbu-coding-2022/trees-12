26cd3baa9afdcfa41c6207e3588a10b5b3f7ca66
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-11 20:55:55 +0300

    feat: Added interface describing bst functions.

---
+++ BinarySearchTrees/src/main/kotlin/binarysearchtrees/BinarySearchTree.kt
@@ -0,0 +1,34 @@
+package binarysearchtrees
+
+interface BinarySearchTree<K : Comparable<K>, V> {
+    val size: Int
+
+    fun isEmpty(): Boolean
+
+    fun clear()
+
+    fun getRoot(): MutableVertex<K, V>?
+
+    operator fun get(key: K): V?
+
+    fun put(key: K, value: V): V?
+
+    fun remove(key: K): V?
+
+    fun remove(key: K, value: V): Boolean
+
+    //for extension functions like all, any, ...
+    interface Vertex<K, V> {
+        val key: K
+        val value: V
+        val left: Vertex<K, V>?
+        val right: Vertex<K, V>?
+    }
+
+    interface MutableVertex<K, V> : Vertex<K, V> {
+        override val left: MutableVertex<K, V>?
+        override val right: MutableVertex<K, V>?
+
+        fun setValue(newValue: V): V
+    }
+}
