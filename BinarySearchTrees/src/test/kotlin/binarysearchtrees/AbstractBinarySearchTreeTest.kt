a7a2566ccbe54796417bd80728583fca84523f41
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-16 03:47:59 +0300

    test: Added test for iterator exceptions.

--- BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
+++ BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
@@ -1,9 +1,10 @@
 package binarysearchtrees
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.random.Random
 
 class AbstractBinarySearchTreeTest {
@@ -36,5 +37,34 @@ class AbstractBinarySearchTreeTest {
             values.reversed().distinctBy { it.first }.sortedBy { it.first },
             listOfPairKeyValue
         )
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [10, 21, 32, 43, 54, 65, -10, -15])
+    fun `Functions of iterator throws exceptions after change tree`(key: Int) {
+        values.forEach { tree.put(it.first, it.second) }
+
+        var iterator = tree.iterator()
+        tree.remove(key)
+        assertThrows(ConcurrentModificationException::class.java) { iterator.hasNext() }
+
+        iterator = tree.iterator()
+        tree.put(key, key * 100)
+        assertThrows(ConcurrentModificationException::class.java) { iterator.next() }
+
+        iterator = tree.iterator()
+        tree.remove(key, key * 100)
+        assertThrows(ConcurrentModificationException::class.java) { iterator.next() }
+
+        iterator = tree.iterator()
+        tree[key] = key * 100
+        assertThrows(ConcurrentModificationException::class.java) { iterator.hasNext() }
+
+        iterator = tree.iterator()
+        tree.clear()
+        assertThrows(ConcurrentModificationException::class.java) { iterator.hasNext() }
+
+        iterator = tree.iterator()
+        assertThrows(NoSuchElementException::class.java) { iterator.next() }
     }
 }
