af174f672d8986135df8e218414ce0e84d776638
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-18 23:33:48 +0300

    test: Added size checks.

--- BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
+++ BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
@@ -37,12 +37,14 @@ class AbstractBinarySearchTreeTest {
             values.reversed().distinctBy { it.first }.sortedBy { it.first },
             listOfPairKeyValue
         )
+        assertEquals(values.distinctBy { it.first }.size, tree.size)
     }
 
     @ParameterizedTest
     @ValueSource(ints = [10, 21, 32, 43, 54, 65, -10, -15])
     fun `Functions of iterator throws exceptions after change tree`(key: Int) {
         values.forEach { tree.put(it.first, it.second) }
+        tree.put(key, 69)
 
         var iterator = tree.iterator()
         tree.remove(key)
@@ -76,25 +78,33 @@ class AbstractBinarySearchTreeTest {
         val expected = key * 198
         tree[key] = expected
         assertEquals(expected, tree.get(key))
+
+        tree.remove(key)
+        assertEquals(null, tree.get(key))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [9, 20, 32, 81, 77, 94, -10, -15])
-    fun `Function remove deletes the element correctly`(key: Int) {
+    fun `Function remove deletes the some element correctly`(key: Int) {
         values.forEach { tree.put(it.first, it.second) }
 
         var value = key * 198
         tree[key] = value
+        var size = tree.size - 1
         assertEquals(value, tree.remove(key))
         assertEquals(null, tree.remove(key))
         assertEquals(null, tree[key])
+        assertEquals(size, tree.size)
         assertTrue(isBinarySearchTree(tree))
 
         value = key * 95
         tree[key] = value
+        size = tree.size - 1
         assertFalse(tree.remove(key, value + 10))
         assertTrue(tree.remove(key, value))
+        assertFalse(tree.remove(key, value))
         assertEquals(null, tree[key])
+        assertEquals(size, tree.size)
         assertTrue(isBinarySearchTree(tree))
     }
 
@@ -107,16 +117,20 @@ class AbstractBinarySearchTreeTest {
             it.setValue(value)
             it.key
         } ?: -25
+        var size = tree.size - 1
         assertEquals(value, tree.remove(oldKey))
         assertNotEquals(oldKey, tree.getRoot()?.key)
+        assertEquals(size, tree.size)
         assertTrue(isBinarySearchTree(tree))
 
         oldKey = tree.getRoot()?.let {
             it.setValue(value)
             it.key
         } ?: -25
+        size = tree.size - 1
         assertTrue(tree.remove(oldKey, value))
         assertNotEquals(oldKey, tree.getRoot()?.key)
+        assertEquals(size, tree.size)
         assertTrue(isBinarySearchTree(tree))
     }
 
@@ -126,6 +140,7 @@ class AbstractBinarySearchTreeTest {
 
         tree.clear()
         assertTrue(tree.isEmpty())
+        assertNull(tree.getRoot())
         assertEquals(0, tree.size)
     }
 }
