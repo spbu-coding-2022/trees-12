fbeee001b4a035abc2c29a42b7b4767c5d84678a
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-18 21:28:08 +0300

    test: Added tests for RBT. Added: - checker of RBT invariant; - added test for search(get) operation; - added test for insert(put, set) operation; - added test for remove operation; - added test for iterator exceptions.

--- BinarySearchTrees/src/test/kotlin/binarysearchtrees/InvariantCheckers.kt
+++ BinarySearchTrees/src/test/kotlin/binarysearchtrees/InvariantCheckers.kt
@@ -7,3 +7,32 @@ fun <K : Comparable<K>, V> isBinarySearchTree(tree: BinarySearchTree<K, V>): Boo
     }
     return tree.getRoot()?.let { checkBSTInvariant(it) } ?: true
 }
+
+fun <K : Comparable<K>, V> isRedBlackTree(tree: RedBlackTree<K, V>): Boolean {
+    var f = true
+
+    fun checkRBTInvariant(vertex: RedBlackTree.Vertex<K, V>): Int {
+        if (vertex.color == RedBlackTree.Vertex.Color.RED) {
+            f = f && (vertex.left?.color != RedBlackTree.Vertex.Color.RED)
+            f = f && (vertex.right?.color != RedBlackTree.Vertex.Color.RED)
+        }
+        val leftBlackHeight = vertex.left?.let {
+            f = f && (vertex.key > it.key)
+            checkRBTInvariant(it)
+        } ?: 1
+        val rightBlackHeight = vertex.right?.let {
+            f = f && (vertex.key < it.key)
+            checkRBTInvariant(it)
+        } ?: 1
+        f = f && (leftBlackHeight == rightBlackHeight)
+        return if (vertex.color == RedBlackTree.Vertex.Color.BLACK)
+            leftBlackHeight + 1
+        else leftBlackHeight
+    }
+
+    return tree.getRoot()?.let {
+        f = (it.color == RedBlackTree.Vertex.Color.BLACK)
+        checkRBTInvariant(it)
+        f
+    } ?: true
+}
