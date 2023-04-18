ce87a5447bda22f14b88438126e13a6edae1c06d
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-18 23:36:55 +0300

    test: Added another test for remove operation. Test checks removal every twentieth element.

--- BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
+++ BinarySearchTrees/src/test/kotlin/binarysearchtrees/AbstractBinarySearchTreeTest.kt
@@ -109,6 +109,24 @@ class AbstractBinarySearchTreeTest {
     }
 
     @Test
+    fun `Function remove deletes the existing element correctly`() {
+        values.forEach { tree.put(it.first, it.second) }
+
+        val elements = mutableListOf<Pair<Int, Int>>()
+        values.reversed().distinctBy { it.first }.forEach { elements.add(it) }
+        elements.shuffle()
+        for (i in 0 until elements.size step 20) {
+            val key = elements[i].first
+            val value = elements[i].second
+            val size = tree.size - 1
+            assertEquals(value, tree.remove(key))
+            assertEquals(null, tree[key])
+            assertEquals(size, tree.size)
+            assertTrue(isBinarySearchTree(tree))
+        }
+    }
+
+    @Test
     fun `Function remove deletes the root element correctly`() {
         values.forEach { tree.put(it.first, it.second) }
 
