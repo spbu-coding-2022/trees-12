d1e71ad5f1f26a5657da07b96492a13dc1a32ac1
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-04-24 21:21:00 +0300

    fix: Vertex interfaces moved to a separate file.

--- BinarySearchTrees/src/main/kotlin/binarysearchtrees/BinarySearchTree.kt
+++ BinarySearchTrees/src/main/kotlin/binarysearchtrees/BinarySearchTree.kt
@@ -20,19 +20,4 @@ interface BinarySearchTree<K : Comparable<K>, V> {
     fun remove(key: K, value: V): Boolean
 
     operator fun iterator(): Iterator<MutableVertex<K, V>>
-
-    //for extension functions like all, any, ...
-    interface Vertex<K, V> {
-        val key: K
-        val value: V
-        val left: Vertex<K, V>?
-        val right: Vertex<K, V>?
-    }
-
-    interface MutableVertex<K, V> : Vertex<K, V> {
-        override val left: MutableVertex<K, V>?
-        override val right: MutableVertex<K, V>?
-
-        fun setValue(newValue: V): V
-    }
 }
