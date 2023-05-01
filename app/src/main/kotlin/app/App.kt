07614b8d236433924ca0235c1c8ad279ca2e3f0d
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-05-01 14:05:13 +0300

    feat: Added function for drawing main window.

--- app/src/main/kotlin/app/App.kt
+++ app/src/main/kotlin/app/App.kt
@@ -1,5 +1,19 @@
 package app
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.application
+import app.view.MainWindow
+
 fun main() {
-    println("Hello World!")
+    application {
+        MaterialTheme(
+            colorScheme = MaterialTheme.colorScheme.copy(
+                primary = Color.Magenta,
+                secondary = Color(162, 32, 240)
+            )
+        ) {
+            MainWindow(::exitApplication)
+        }
+    }
 }
