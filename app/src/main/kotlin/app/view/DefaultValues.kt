83d03f7a646b8124ffc63e85e1502f2d3f4ea077
Arsene-Baitenov <arsenebaitenov@gmail.com>
2023-05-02 12:57:06 +0300

    feat: Added file with the constants.

---
+++ app/src/main/kotlin/app/view/DefaultValues.kt
@@ -0,0 +1,54 @@
+package app.view
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val defaultPadding = 5.dp
+
+val defaultScrollDelta = ScrollDelta(0.dp, 0.dp)
+
+const val defaultScrollCf = 75
+
+val defaultVertexSize = 60.dp
+
+val defaultBrush
+    @Composable
+    get() = Brush.linearGradient(listOf(MaterialTheme.colorScheme.primary, MaterialTheme.colorScheme.secondary))
+
+val defaultVVBrush
+    @Composable
+    get() = Brush.linearGradient(listOf(MaterialTheme.colorScheme.secondary, MaterialTheme.colorScheme.primary))
+
+val defaultBlackBrush
+    @Composable
+    get() = Brush.linearGradient(listOf(Color.Black, Color.Black))
+
+val defaultRedBrush
+    @Composable
+    get() = Brush.linearGradient(listOf(Color(139, 0, 0), Color(139, 0, 0)))
+
+val defaultEdgeColor
+    @Composable
+    get() = MaterialTheme.colorScheme.tertiary
+
+const val defaultEdgeWidht = 2f
+
+val defaultBackground
+    @Composable
+    get() = MaterialTheme.colorScheme.background
+
+val defaultTextStyle
+    @Composable
+    get() = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary)
+
+val defaultLargeTextStyle
+    @Composable
+    get() = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.primary, fontSize = 20.sp)
+
+val defaultOnPrimaryLargeTextStyle
+    @Composable
+    get() = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onPrimary, fontSize = 20.sp)
